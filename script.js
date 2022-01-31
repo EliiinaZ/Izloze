b7af506673338b261cf5fd23ada3badab163cdc8
EliiinaZ <forgameplay1122@gmail.com>
2022-01-31 15:04:21 +0200

    Otrais1

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const balvas=['Dators', 'Veļasmašīna', 'Viedtalrunis', 'Automašīna', 'Trauk
 const naudaKopa=1000000;//kopēja summa izlozei
 let uzvaretajuSkaits=5;
 let rand= Math.random()*5;
-rand= Math.floor(rand);//noapaļo skaitļus uz leju
-console.log(vardi[rand]);//izvada kosole, nejauši izvēlētus vārdus
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = '';
+for (let i=0; i< uzvaretajuSkaits; i++){
+    let rand=Math.random() * vardi.length;
+    rand= Math.floor(rand);//noapaļo skaitļus uz leju
+   
+    let uzvaretajs = vardi[rand];
+    rindas.innerHTML +=`
+    <tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>
+    </tr>`
+}
